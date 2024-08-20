ae9fc417c841ccc1415cd8e99d6e99450b245f1b
Qasid Saleh <qasid.developer@gmail.com>
2024-08-20 16:26:35 -0700

    Implemented basic widget functionality.

--- wp-content/plugins/user-preferences-api-fetch/includes/class-upaf-widget.php
+++ wp-content/plugins/user-preferences-api-fetch/includes/class-upaf-widget.php
@@ -7,7 +7,16 @@ class UPAF_Widget extends WP_Widget {
             array('description' => __('Displays data based on user preferences.', 'upaf'))
         );
     }
+    // Placeholder for actual widget content
     public function widget($args, $instance) {
+        if (!is_user_logged_in()) {
+            echo '<p>' . __('Please log in to view this widget.', 'upaf') . '</p>';
+            return;
+        }
+        // Placeholder for actual widget content
+        echo $args['before_widget'];
+        echo $args['before_title'] . __('User Preferences', 'upaf') . $args['after_title'];
         echo '<p>' . __('Widget is functional.', 'upaf') . '</p>';
+        echo $args['after_widget'];
     }
 }

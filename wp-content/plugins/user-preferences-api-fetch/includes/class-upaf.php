f10eca22a1087333974c08db1d4c095290578d42
Qasid Saleh <qasid.developer@gmail.com>
2024-08-20 16:30:21 -0700

    Added custom rewrite endpoint for user preferences

--- wp-content/plugins/user-preferences-api-fetch/includes/class-upaf.php
+++ wp-content/plugins/user-preferences-api-fetch/includes/class-upaf.php
@@ -1,6 +1,9 @@
 <?php
 class UPAF_User_Preferences_API_Fetch {
     public function __construct() {
-        // Actions will be added later
+        add_action('init', array($this, 'add_rewrite_endpoint'));
+    }
+    public function add_rewrite_endpoint() {
+        add_rewrite_endpoint('upaf_preferences', EP_ROOT | EP_PAGES);
     }
 }

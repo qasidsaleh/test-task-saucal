647f2c322c35a20963fa04db89af12c7f1808f69
Qasid Saleh <qasid.developer@gmail.com>
2024-08-20 21:39:35 -0700

    added save preferences functionality

--- wp-content/plugins/user-preferences-api-fetch/includes/class-upaf.php
+++ wp-content/plugins/user-preferences-api-fetch/includes/class-upaf.php
@@ -7,6 +7,8 @@ class UPAF_User_Preferences_API_Fetch {
         add_action('init', array($this, 'add_rewrite_endpoint'));
         add_action('woocommerce_account_upaf_preferences_endpoint', array($this, 'upaf_preferences_page'));
         add_action('template_redirect', array($this, 'handle_form_submission'));
+        add_action('wp_ajax_upaf_save_preferences', array($this, 'save_preferences'));
+        add_action('wp_ajax_nopriv_upaf_save_preferences', array($this, 'save_preferences'));
         add_action('woocommerce_account_menu_items', array($this, 'add_my_account_link'), 40);
         add_filter('woocommerce_get_endpoint_url', array($this, 'add_my_account_link'), 10, 2);
     }
@@ -40,6 +42,27 @@ class UPAF_User_Preferences_API_Fetch {
         if (isset($_POST['upaf_save_preferences']) && is_user_logged_in()) {
             $this->save_preferences();
         }
+    }
+    // Save preferences functionality
+    public function save_preferences() {
+        if (!isset($_POST['preferences']) || !wp_verify_nonce($_POST['_wpnonce'], 'upaf_save_preferences')) {
+            wp_die(__('Invalid nonce', 'upaf'));
+        }
+    
+        $user_id = get_current_user_id();
+        $preferences = sanitize_text_field($_POST['preferences']);
+        $preferences_array = array_filter(array_map('sanitize_text_field', array_map('trim', explode(',', $preferences))));
+    
+        if (empty($preferences_array)) {
+            wp_die(__('No valid preferences provided', 'upaf'));
+        }
+    
+        // Save preferences to user meta
+        update_user_meta(get_current_user_id(), '_upaf_preferences', $preferences_array);
+    
+        // Redirect to avoid form resubmission
+        wp_redirect(wc_get_account_endpoint_url('upaf_preferences'));
+        exit;
     }
     // Show user preferences in WooCommerce My Account
     public function add_my_account_link($items) {

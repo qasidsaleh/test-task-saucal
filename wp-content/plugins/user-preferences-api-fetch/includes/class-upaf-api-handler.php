0bc0acd3ad4b37d7b5adf3cd331fd9d543aff8e4
Qasid Saleh <qasid.developer@gmail.com>
2024-08-20 17:12:14 -0700

    Added a placeholder for the API handler class

---
+++ wp-content/plugins/user-preferences-api-fetch/includes/class-upaf-api-handler.php
@@ -0,0 +1,10 @@
+<?php
+
+class UPAF_API_Handler {
+
+    private $api_url = 'https://httpbin.org/post';
+
+    public function fetch_data($preferences) {
+        // API fetching logic will be added here later
+    }
+}
